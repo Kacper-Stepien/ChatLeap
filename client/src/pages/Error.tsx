6afb027f65af1fcbf034499ed7864192faf50d1e
Kacper Stępień <kacper2007x48@gmail.com>
2023-06-27 22:18:17 +0200

    refactor Error.tsx page, add redirection to home page after 5 seconds

--- client/src/pages/Error.tsx
+++ client/src/pages/Error.tsx
@@ -1,4 +1,5 @@
-import React, { useContext } from "react";
+import React, { useContext, useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 
 import { ThemeContext } from "../context/ThemeContext";
 
@@ -8,10 +9,36 @@ const ErrorPage: React.FC = () => {
   const { mode, accent } = useContext(ThemeContext);
   const theme = mode + accent;
   const styleClasses = [styles[theme], styles.error];
+
+  const [seconds, setSeconds] = useState<number>(5);
+  const navigate = useNavigate();
+
+  const redirectToHomePage = () => {
+    navigate("/");
+  };
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      if (seconds > 1) {
+        setSeconds((prevSecond) => prevSecond - 1);
+      } else {
+        redirectToHomePage();
+      }
+    }, 1000);
+
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [seconds]);
+
   return (
     <div className={styleClasses.join(" ")}>
       <h1>404</h1>
       <p>Page not found</p>
+      <p>
+        You will be redirected to the home page in <span>{seconds}</span>{" "}
+        seconds
+      </p>
     </div>
   );
 };
